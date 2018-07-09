038692635a70da2d988b28229d4e7483aac96f44
ONosova <nosova-olenka@mail.ru>
2018-07-10 01:01:18 +0300

    up 9

--- code.py
+++ code.py
@@ -1,12 +1,36 @@
 if __name__ == '__main__':
-    a = [
-        'aaaaaaaaaaaaaaaaaaaa','bbbbbbbbbbbbbbbbbbb','ccccccccccccccc','ddddddddddddddddddddd','eeeeeeeeeeeeeeeeeeeeeeee']
+    a = ['aaaaaaaaaaaaaaaaaaaa','bbbbbbbbbbbbbbbbbbb','ccccccccccccccc','ddddddddddddddddddddd','eeeeeeeeeeeeeeeeeeeeeeee']
 
     num = 10
+
+
+
+
+
+
+
+
+
+
+
+
 
     string = 'my str'
 
     string2 = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 0, 1, 2], [3, 4, 5, 6, 7, 8]]
+
+
+
+
+
+
+
+
+
+
+    a=5
+    b=6
+
 
     print("lala")
 
