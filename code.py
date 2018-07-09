48ff2a90cd2d4d001974b446b5a89dc23f1521a1
ONosova <nosova-olenka@mail.ru>
2018-07-10 00:35:35 +0300

    up 3

--- code.py
+++ code.py
@@ -1,5 +1,25 @@
 if __name__=='__main__':
-    print("Hello word")
-    print("Update Hello word")
-    print("Update Hello word 1")
-    print("Update Hello word 2")
+    a = ['aaaaaaaaaaaaaaaaaaaa','bbbbbbbbbbbbbbbbbbb', 'ccccccccccccccc','ddddddddddddddddddddd','eeeeeeeeeeeeeeeeeeeeeeee']
+
+
+
+
+
+
+
+
+
+
+
+
+
+    num = 10
+
+    string = 'my str'
+
+    string2 = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 0, 1, 2], [3, 4, 5, 6, 7, 8]]
+
+    print(string)
+
+    print(string2)
+    print(a)
